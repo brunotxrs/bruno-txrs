e9052e8927a227982a1959eaa18245cf01a498e0
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-09-19 21:20:36 -0400

    :sparkles: feat: create scripts for use SDKs of Firebase

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAeSeu9s_CaKwSLijJUA_y1RTdB0mHtv18",
+  authDomain: "brunotxrs-dev-portfolio.firebaseapp.com",
+  projectId: "brunotxrs-dev-portfolio",
+  storageBucket: "brunotxrs-dev-portfolio.firebasestorage.app",
+  messagingSenderId: "271114833056",
+  appId: "1:271114833056:web:16bf4b111e50eea6dd0dc5",
+  measurementId: "G-R65JEPZ5JB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
