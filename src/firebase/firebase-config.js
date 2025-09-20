2eab60f1f9d840c3be76821065f42dfe128ea142
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-09-19 21:45:59 -0400

    :lock: fix: Hide Firebase API key with .env

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAeSeu9s_CaKwSLijJUA_y1RTdB0mHtv18",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "brunotxrs-dev-portfolio.firebaseapp.com",
   projectId: "brunotxrs-dev-portfolio",
   storageBucket: "brunotxrs-dev-portfolio.firebasestorage.app",
